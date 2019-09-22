75e4c2222a0041294c935f89a732b716a8d25b5d
Konrad Szydlo <konrad_szydlo@yahoo.co.uk>
2019-09-22 23:16:57 +0200

    Added Chapter 8 code

---
+++ chapter8/activity1/repl.clj
@@ -0,0 +1,7 @@
+(use '[clojure.string :only (capitalize)])
+
+(capitalize "name")
+
+(use '[clojure.set :exclude (join)])
+
+(subset? #{1 2} #{1 2 3})
---
+++ chapter8/exercise10/hello-leiningen/src/hello_leiningen/core.clj
@@ -0,0 +1,8 @@
+(ns hello-leiningen.core
+
+  (:gen-class))
+
+(defn -main
+  "Running app with profiles"
+  [& args]
+  (println "Using profiles"))
