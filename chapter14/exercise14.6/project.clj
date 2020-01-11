aee548acb1adfc77b17a2286cf6662fa4b077a53
scott <scottmccaughie@gmail.com>
2020-01-11 17:41:41 +0000

    add exercise14.6 test

---
+++ chapter14/exercise14.6/project.clj
@@ -0,0 +1,18 @@
+(defproject exercise14.6 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-http "3.10.0"]
+                 [clojure.java-time "0.3.2"]
+                 [hikari-cp "2.8.0"]
+                 [org.apache.derby/derby "10.14.2.0"]
+                 [org.clojure/clojure "1.10.1"]
+                 [org.clojure/data.csv "0.1.4"]
+                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/java.jdbc "0.7.9"]
+                 [compojure "1.6.1"]
+                 [metosin/muuntaja "0.6.4"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
+                 [semantic-csv "0.2.1-alpha1"]])
