db52252f7f735c4908368a6df73b4349d16eebc6
scott <scottmccaughie@gmail.com>
2020-01-11 15:36:26 +0000

    add exercise14.1 test

---
+++ chapter14/exercise14.1/project.clj
@@ -0,0 +1,8 @@
+(defproject exercise14.1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-http "3.10.0"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]])
