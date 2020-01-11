f17564a50567680192dd5b411a199f5c77928a04
scott <scottmccaughie@gmail.com>
2020-01-11 16:18:38 +0000

    add activity14.3 test

---
+++ chapter14/exercise14.3/project.clj
@@ -0,0 +1,10 @@
+(defproject exercise14.3 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[clj-http "3.10.0"]
+                 [compojure "1.6.1"]
+                 [metosin/muuntaja "0.6.4"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]])
